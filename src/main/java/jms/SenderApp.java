94a93f08433aaf5e97050b84569ef62a0fe4d980
Piotr Marecki <pmarecki@gmail.com>
2017-11-23 12:47:34 +0100

    Update SenderApp.java

--- src/main/java/jms/SenderApp.java
+++ src/main/java/jms/SenderApp.java
@@ -1,22 +1,42 @@
 package jms;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
 import javax.jms.TextMessage;
 import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+class MyMessageCreator implements MessageCreator {
+    AtomicInteger counter = new AtomicInteger();
+
+    @Override
+    public Message createMessage(Session session) throws JMSException {
+        TextMessage m = session.createTextMessage();
+        m.setJMSCorrelationID("000" + counter.incrementAndGet());
+//        m.setJMSReplyTo(jmsTemplate.getDefaultDestination());
+        m.setJMSPriority(9);
+        return m;
+    }
+}
 
 @SpringBootApplication
 @EnableJms
 @EnableWebMvc
+@Slf4j
 @ImportResource("classpath:sender-receiver-jms.xml")
 public class SenderApp {
 
@@ -25,27 +45,16 @@ public class SenderApp {
         ConfigurableApplicationContext ctx = SpringApplication.run(SenderApp.class, args);
 
         JmsTemplate jmsTemplate = ctx.getBean(JmsTemplate.class);
-//        jmsTemplate.setDeliveryPersistent(false);
+        jmsTemplate.getSessionAcknowledgeMode();
+        jmsTemplate.setDeliveryPersistent(false);
 
-//        MessageListenerContainer container = ctx.getBean("jmsContainer1", MessageListenerContainer.class);
-//        container.stop();
+        MessageCreator mc = new MyMessageCreator();
 
-//        jmsTemplate.send("box1", session -> session.createTextMessage("ping!"));
-//        jmsTemplate.send("box1", session -> session.createTextMessage("ping!"));
-//        jmsTemplate.send("box1", session -> session.createTextMessage("ping!"));
-        for (int i = 0; i < 1000000; i++) {
-            Integer ii = i;
+        for (int i = 0; i < 1_000_000; i++) {
             if (i%100==0) System.out.println(i);
-            jmsTemplate.send("box1", session -> {
-                TextMessage m = session.createTextMessage();
-                m.setJMSCorrelationID("000" + ii);
-                m.setJMSReplyTo(jmsTemplate.getDefaultDestination());
-                m.setJMSPriority(9);
-                return m;
-            });
+            jmsTemplate.send("xxx_topic", mc);
         }
         Thread.sleep(1000);
-        jmsTemplate.convertAndSend("box1", Arrays.asList(1,2,3));
         ctx.close();
 
     }
